45240f26e88914f9f9be6c34a1597cee01f1efda
reymundvirtus <reymundvirtus@outlook.com>
2022-07-20 18:28:37 +0800

    modify favicon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -11,6 +11,8 @@
     <meta name="description" content="Simple landind page" />
     <meta name="keywords" content="" />
     <meta name="author" content="" />
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
     <!--Replace with your tailwind.css once created-->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
